0aad8207f177d1653a0854e4a8707d88854f0023
rippe <rp.nykanen@gmail.com>
2019-04-21 12:06:11 +0300

    handle urls on message

--- src/chat/components/MessageList.js
+++ src/chat/components/MessageList.js
@@ -13,7 +13,7 @@ const MessageList = () => {
   return <ul className={css.messageList}>
     {messages.map((item, index) => {
 
-      const {author, content: message, mentions} = item
+      const {author, content: message, mentions, embeds} = item
 
       const users = mentions.users.reduce( (acc,user) => {
         const temp = {}
@@ -34,7 +34,8 @@ const MessageList = () => {
         const currentDay = new Date(item.createdTimestamp)
         dayText = currentDay.toLocaleDateString()
       }
-      const parsedMessage = useridToUsername(message,users)
+      //const parsedMessage = useridToUsername(message,users)
+      const parsedMessage = handleMessages(message, users, embeds)
       return <Message
         key={item.id} 
         header={dayText} 
@@ -46,16 +47,30 @@ const MessageList = () => {
   </ul>
 }
 
+const handleMessages = (message, users, urls) => {
+  //https://www.npmjs.com/package/react-process-string
+  //message = handleUrls(message, urls)
+  message = useridToUsername(message, users)
+
+  return message;
+
+}
+
+const handleUrls = (message, embeds) => {
+  let msg = message;
+  embeds.forEach( (embed) => msg = msg.replace(embed.url, `<a href="${embed.url}">${embed.url}</a>`) )
+  //msg = msg.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+  return msg
+}
+
 const useridToUsername = (message, users) => {
-    const regex = new RegExp('<@\\d*>', 'g');
-    const userids = message.match(regex);
-    if( userids != null && userids.length != 0){
-        userids.forEach( userid => {
-            message = message.replace(userid, '@'+users[userid.substring(2, (userid.length - 1))])
-            }
-        )
-    }
-    return message
+  const regex = new RegExp('<@\\d*>', 'g');
+  const userids = message.match(regex);
+  if( userids != null && userids.length != 0){
+      userids.forEach( userid => message = message.replace(userid, '@'+users[userid.substring(2, (userid.length - 1))])
+      )
+  }
+  return message
 }
 
 
